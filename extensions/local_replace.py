0fcaaa684d3df014de045b31184026c58b72f637
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2015-07-28 13:21:59 +0200

    OSM browse link and JOSM remote control edit link.

--- extensions/local_replace.py
+++ extensions/local_replace.py
@@ -20,20 +20,45 @@ sub_nolinks = [
 ]
 
 def prettify(message):
+    if not message:
+	return ''
     for cre, replacement in sub_links + sub_nolinks:
         message = cre.sub(replacement, message)
 
     return message
 
 def prettify_nolink(message):
+    if not message:
+	return ''
     for cre, replacement in sub_nolinks:
         message = cre.sub(replacement, message)
 
-    return message
+def location_link(issue):
+    if not issue:
+	return ''
+    dist = 400 # metres
+    left = float(issue.lon) - 360.0 * (dist / 0.5 / 40000000)
+    right = float(issue.lon) + 360.0 * (dist / 0.5 / 40000000)
+    top = float(issue.lat) + 360.0 * (dist / 40000000)
+    bottom = float(issue.lat) - 360.0 * (dist / 40000000)
+    src = 'remonty.openstreetmap.pl%2Fremonty%2Fissue' + str(issue.id)
+
+    osmlink  = 'http://osm.org/#map=' + str(issue.z) + '/' + \
+	 str(issue.lat) + '/' + str(issue.lon)
+    josmlink = 'http://127.0.0.1:8111/load_and_zoom?left=' + str(left) + \
+	'&right=' + str(right) + '&top='  + str(top) + \
+	'&bottom=' + str(bottom) + '&changeset_source=' + src + \
+	'&changeset_comment=Aktualizacja%20remontu'
+
+    return '<a href="issue' + str(issue.id) + '">' + \
+	str(issue.location) + '</a><span class="permalinks">' + \
+	'(<a href="' + osmlink + '">osm</a>)' + \
+	'(<a href="' + josmlink + '">JOSM</a>)' + '</span>'
 
 def init(instance):
     instance.registerUtil('prettify', prettify)
     instance.registerUtil('prettify_nolink', prettify_nolink)
+    instance.registerUtil('location_link', location_link)
 
 if "__main__" == __name__:
     print " user:222", prettify(" user:222")
